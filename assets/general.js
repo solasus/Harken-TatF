344244fdccdc7abd2c2fee35a8f1bdc4be4f1db2
Kevin Brady <kevin@solasus.com>
2016-08-16 23:51:12 -0400

    rollback

--- assets/general.js
+++ assets/general.js
@@ -1,17 +1,18 @@
 jQuery( document ).ready(function( $ ) {
 	
-	function loadFrame() {
+//	function loadFrame() {
 		var iframe = document.getElementById("appframe");
 		iframe.onload = function(){
 			iframe.contentWindow.cordova = window.cordova;
 		};
 		iframe.src = 'https://www.harken.com/apps/tradeapp/';
-	}
+//	}
 
 	function pageSize() {
 		var pgWidth = $(window).width();
 		var pgHeight = $(window).height();
 		$('.page').css({'width':pgWidth+'px','height':pgHeight+'px'});
+		$('#appframe').css({'width':pgWidth+'px','height':pgHeight+'px'});
 	}
 	pageSize();
 
@@ -19,9 +20,9 @@ jQuery( document ).ready(function( $ ) {
 		var pgWidth = $(window).width();
 		var pgHeight = $(window).height();
 		$('#appframe').css({'width':pgWidth+'px','height':pgHeight+'px'});
-		loadFrame();
+//		loadFrame();
 	}
-	setTimeout(appSize, 3000);
+//	setTimeout(appSize, 3000);
 
 	var resizeId;
 	$(window).resize(function() {
